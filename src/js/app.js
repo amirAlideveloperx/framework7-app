4e8badcc581aa851c6c4336b7d513a92ab86b9c7
ehsanghaffarii <ghafari.5000@gmail.com>
2022-06-10 08:28:15 +0430

    add

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+import $ from 'dom7';
+import Framework7, { getDevice } from './framework7-custom.js';
+
+// Import F7 Styles
+import '../css/framework7-custom.less';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.less';
+// Import Cordova APIs
+import cordovaApp from './cordova-app.js';
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import main app component
+import App from '../app.f7';
+
+var device = getDevice();
+var app = new Framework7({
+  name: 'testApp', // App name
+  theme: 'auto', // Automatic theme detection
+  el: '#app', // App root element
+  component: App, // App main component
+  id: 'io.framework7.myapp', // App bundle ID
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+  // Register service worker (only on production build)
+  serviceWorker: process.env.NODE_ENV ==='production' ? {
+    path: '/service-worker.js',
+  } : {},
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: device.cordova && !device.electron,
+    scrollIntoViewCentered: device.cordova && !device.electron,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+    },
+  },
+});
